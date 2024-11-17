894973987c7e00aba7ac9e8a5fa24244dba928bf
gsantoro <giuseppe.santoro@elastic.co>
2024-11-17 10:58:20 +0000

    conditional template for python

--- {{ cookiecutter.project_slug }}/devenv.nix
+++ {{ cookiecutter.project_slug }}/devenv.nix
@@ -6,4 +6,16 @@
     pkgs.git
     pkgs.gum
   ];
+
+  {% if cookiecutter.python == 'y' %}
+  languages.python = {
+    enable = true;
+    package = pkgs.python312;
+    uv = {
+      enable = true;
+      sync.enable = true;
+    };
+    venv.enable = true;
+  };
+  {% endif %}
 }
